dddef42d59dc3af1cd2a43b311774dc286251d15
Emre Unal <emreunal99@gmail.com>
2015-08-23 00:02:07 +0300

    Remove unused container in instance initializer

--- app/instance-initializers/active-model-adapter.js
+++ app/instance-initializers/active-model-adapter.js
@@ -4,23 +4,17 @@ import ActiveModelSerializer from 'active-model-adapter/active-model-serializer'
 export default {
   name: 'active-model-adapter',
   initialize: function(applicationOrRegistry) {
-    var registry, container;
-    if (applicationOrRegistry.registry && applicationOrRegistry.container) {
+    var registry;
+    if (applicationOrRegistry.registry) {
       // initializeStoreService was registered with an
       // instanceInitializer. The first argument is the application
       // instance.
       registry = applicationOrRegistry.registry;
-      container = applicationOrRegistry.container;
     } else {
       // initializeStoreService was called by an initializer instead of
       // an instanceInitializer. The first argument is a registy. This
       // case allows ED to support Ember pre 1.12
       registry = applicationOrRegistry;
-      if (registry.container) { // Support Ember 1.10 - 1.11
-        container = registry.container();
-      } else { // Support Ember 1.9
-        container = registry;
-      }
     }
 
     registry.register('adapter:-active-model', ActiveModelAdapter);
