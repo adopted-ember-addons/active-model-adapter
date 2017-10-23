1ef164120b2079f5e8b7c1a69daf3746c80e0d45
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-10-23 12:06:47 +0200

    Update "ember-cli-qunit" to v4.0.1

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,10 +1,11 @@
 import resolver from './helpers/resolver';
 import DS from 'ember-data';
-import {
-  setResolver
-} from 'ember-qunit';
+import { setResolver } from 'ember-qunit';
+import { start } from 'ember-cli-qunit';
 
 DS.ActiveModelAdapter = null;
 DS.ActiveModelSerializer = null;
 
 setResolver(resolver);
+
+start();
