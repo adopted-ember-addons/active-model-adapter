428e48028a04b580217bbcfe09baf34c1a0f04b6
Martin Feckie <mfeckie@gmail.com>
2019-10-14 22:57:33 +0800

    Add generated types from ember ts:precompile

---
+++ initializers/active-model-adapter.d.ts
@@ -0,0 +1,6 @@
+import Application from '@ember/application';
+export declare function initialize(application: Application): void;
+declare const _default: {
+    initialize: typeof initialize;
+};
+export default _default;
