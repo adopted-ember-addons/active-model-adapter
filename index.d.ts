7fbb2dabdc0c44784990e7ca05de343ea3fb4c98
Martin Feckie <mfeckie@gmail.com>
2019-10-14 22:57:33 +0800

    Add generated types from ember ts:precompile

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import ActiveModelAdapter from './active-model-adapter';
+import ActiveModelSerializer from './active-model-serializer';
+export default ActiveModelAdapter;
+export { ActiveModelAdapter, ActiveModelSerializer };
