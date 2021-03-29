eb6b0566579e7cc95230683fef6512089cd764ce
James Herdman <james.herdman@gmail.com>
2021-03-29 13:26:30 +0000

    Remove Inactive LTS's from Test Matrix

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,20 +7,20 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.12',
+        name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
-            'ember-source': '~3.12.0',
-            'ember-data': '~3.12.0'
+            'ember-source': '~3.20.0',
+            'ember-data': '~3.20.0'
           }
         }
       },
       {
-        name: 'ember-lts-3.16',
+        name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~3.16.0',
-            'ember-data': '~3.16.0'
+            'ember-source': '~3.24.0',
+            'ember-data': '~3.24.0'
           }
         }
       },
