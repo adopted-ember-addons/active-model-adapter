211c0c5132fff3e85db9fdb4ffe75b30f52d6340
James Herdman <james.herdman@gmail.com>
2021-04-16 23:07:12 +0000

    Drop Any d.ts Files

--- initializers/active-model-adapter.d.ts
+++
@@ -1,6 +0,0 @@
-import Application from '@ember/application';
-export declare function initialize(application: Application): void;
-declare const _default: {
-  initialize: typeof initialize;
-};
-export default _default;
--- types/global.d.ts
+++
@@ -1,6 +0,0 @@
-// Types for compiled templates
-declare module 'active-model-adapter/templates/*' {
-  import { TemplateFactory } from 'htmlbars-inline-precompile';
-  const tmpl: TemplateFactory;
-  export default tmpl;
-}
