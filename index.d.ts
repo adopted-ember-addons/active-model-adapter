69cf5d621a1cb26cfefdc22f1d6890d6c2721113
James Herdman <james.herdman@gmail.com>
2021-06-13 15:50:30 +0000

    Remove Generated Files

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import ActiveModelAdapter from './active-model-adapter';
-import ActiveModelSerializer from './active-model-serializer';
-export default ActiveModelAdapter;
-export { ActiveModelAdapter, ActiveModelSerializer };
-export declare type AnyObject = Record<string, unknown>;
