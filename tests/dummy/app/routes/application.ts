7719c7543e01130d48ffc256da354b660e340b6d
James Herdman <james.herdman@gmail.com>
2022-02-02 13:51:19 -0500

    Fix Modern Ember Failures

--- tests/dummy/app/routes/application.ts
+++ tests/dummy/app/routes/application.ts
@@ -1,8 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import DS from 'ember-data';
 import type Car from 'dummy/models/car';
+import type Store from 'ember-data/store';
 
 export default class Application extends Route {
+  @service('store') declare store: Store;
+
   model(): DS.PromiseArray<Car> {
     return this.store.findAll('car');
   }
